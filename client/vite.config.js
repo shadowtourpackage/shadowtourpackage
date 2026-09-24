5440a92baddffc58de8f37817dd425cae20672db
harshuuuuuuuuuuuuuuu <harshithts.dev@gmail.com>
2026-09-24 08:23:29 +0530

    for github deploy

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/shadow/', // Replace with your exact GitHub repository name
+  base: '/shadowtourpackage/', // Replace with your exact GitHub repository name
 })
