7b5efbb0e8d5746ab2cb1e235614774b17514d19
harshuuuuuuuuuuuuuuu <harshithts.dev@gmail.com>
2026-09-24 08:39:29 +0530

    git push checkpitn

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/shadowtourpackage/', // Replace with your exact GitHub repository name
+  base: '/', // Replace with your exact GitHub repository name
 })
